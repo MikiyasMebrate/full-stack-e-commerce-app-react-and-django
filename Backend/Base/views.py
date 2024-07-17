c9daf326578d3a539d39493b29c90a0cffe9d684
mikiyasmebrate <mikiyasmebrate@gmail.com>
2024-07-17 20:04:01 +0300

    category list api added

--- Backend/Base/views.py
+++ Backend/Base/views.py
@@ -1,5 +1,19 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from .models import Category, Product
+from rest_framework.decorators import api_view
+from rest_framework import status 
+
+
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+@api_view(['GET'])
+def category_list(request):
+    if request.method == "GET":
+        categories = list(Category.objects.all().values())
+        context = {
+            'categories': categories
+        }
+        return JsonResponse(context)
+    else:
+        return JsonResponse({'message': 'Method not allowed'}, status=405)
+
